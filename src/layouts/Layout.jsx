7d16d03c4fda6e86f46e2ab869b475b8ebcf0fc1
iamBijoyKar <bijoypoco12345@gmail.com>
2023-12-12 22:42:04 +0530

    [refactor]: code clean up and project reset

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
-import NavBar from "../components/NavBar";
-import Header from "../components/Header";
 
 export default function Layout({ children }) {
-  const location = useLocation();
-  const { colors, theme } = useSelector((state) => state.theme);
   return (
-    <div className=" min-h-[130vh]" style={{ backgroundColor: theme == 'light' ? '#ffffff' : '#000000' }} >
-      {location.pathname !== "/" && <Header />}
-      <NavBar />
-      <div className="">{children}</div>
+    <div className=" flex flex-col items-center justify-between min-h-screen" >
+      <h1 className="text-2xl">Header</h1>
+      <main className="">{children}</main>
+      <h1 className="text-2xl">Footer</h1>
     </div>
   );
 }
