c408f86f9ca592ef548a43c80aae88f192d9bb1d
iamBijoyKar <bijoypoco12345@gmail.com>
2023-12-12 22:54:30 +0530

    [build,fix]: build packages fix, tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-export default withMT({
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
