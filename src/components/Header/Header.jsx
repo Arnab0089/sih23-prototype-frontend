b6185e0222e697d4a7329e3771b2c47aba795c29
Sannik Bej <75433730+sannik-bej@users.noreply.github.com>
2023-12-27 00:09:56 +0530

    Added and modified Community page
    Post component added, Icons changed in Navbar, Refactored code.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,3 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faBell } from '@fortawesome/free-solid-svg-icons';
-
-// // app header component for all pages
-// export default function Header({ headerText }) {
-//   return (
-//     <header className="w-full flex items-center justify-between px-3 py-4 bg-[#e6efff]">
-//       <h3 className="text-2xl font-medium text-[#A13131]">{headerText}</h3>
-//       <button className="">
-//         <FontAwesomeIcon className="text-2xl" icon={faBell} />
-//       </button>
-//     </header>
-//   );
-// }
-
 import React from 'react';
 import { PiBellDuotone } from 'react-icons/pi';
 import PropTypes from 'prop-types';
