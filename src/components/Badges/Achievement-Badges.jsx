64afb81743b744a4ef260b6402e7c22fbd8a84ae
Parijat <parijatdas823@gmail.com>
2023-12-26 23:56:31 +0530

    picture added

--- src/components/Badges/Achievement-Badges.jsx
+++ src/components/Badges/Achievement-Badges.jsx
@@ -9,39 +9,41 @@ export function AchievementBadges() {
       <h2 className="text-lg mx-4 my-2 text-primary-text font-bold">
         My Achievements
       </h2>
-      <div className="relative w-full h-[150px] overflow-x-scroll gap-1 mx-4 flex flex-row">
+      <div className="relative w-full h-[150px] overflow-x-scroll gap-1 mx-4 text-sm flex flex-row">
         <div
-          className="flex flex-col items-center justify-center text-center bg-[#ffd0d0] rounded-md p-4"
+          className="flex flex-col items-center justify-around text-center bg-[#ffd0d0] rounded-md p-4"
           style={{ minWidth: '150px' }}
         >
           <p>Safety Advocate </p>
           <img src={Bronze} className="w-100%" />
         </div>
         <div
-          className="flex flex-col items-center justify-center text-center bg-[#ffd0d0] rounded-md p-4"
+          className="flex flex-col items-center justify-around text-center bg-[#ffd0d0] rounded-md p-4"
           style={{ minWidth: '150px' }}
         >
           <p>Bug Buster </p>
           <img src={Gold} className="w-100%" />
         </div>
         <div
-          className="flex flex-col items-center justify-center text-center bg-[#ffd0d0] rounded-md p-4"
+          className="flex flex-col items-center justify-around text-center bg-[#ffd0d0] rounded-md p-4"
           style={{ minWidth: '150px' }}
         >
           <p>Consistent Connector</p>
           <img src={Silver} className="w-100%" />
         </div>
         <div
-          className="flex flex-col items-center justify-center text-center bg-[#ffd0d0] rounded-md p-4"
+          className="flex flex-col items-center justify-around text-center bg-[#ffd0d0] rounded-md p-4"
           style={{ minWidth: '150px' }}
         >
           <p>Spread the Safety </p>
+          <img src={Gold} className="w-100%" />
         </div>
         <div
-          className="flex flex-col items-center justify-center text-center bg-[#ffd0d0] rounded-md p-4"
+          className="flex flex-col items-center justify-around text-center bg-[#ffd0d0] rounded-md p-4"
           style={{ minWidth: '150px' }}
         >
           <p>Verified Pepper </p>
+          <img src={Bronze} className="w-100%" />
         </div>
       </div>
     </>
