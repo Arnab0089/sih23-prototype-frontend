f95c40f4f7edcd7e0832cb36a11886fded536d86
Bijoy Kar <bijoypoco12345@gmail.com>
2024-01-03 16:30:01 +0530

    [feat]: New Post modal added

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import Header from '../components/Header/Header';
 import Navigation from '../components/Navigation/Navigation';
+import NewPostModal from '../components/Modals/NewPost';
 
 // base layout component for all pages in the app
 export default function Layout({ children }) {
+  const [showNewPostModal, setShowNewPostModal] = useState(false);
   return (
     <div className=" flex flex-col items-center justify-between h-screen min-h-screen">
+      <div className="">
+        <NewPostModal
+          showNewPostModal={showNewPostModal}
+          setShowNewPostModal={setShowNewPostModal}
+        />
+      </div>
       <main className="overflow-y-auto w-full h-full">{children}</main>
-      <Navigation />
+      <Navigation
+        showNewPostModal={showNewPostModal}
+        setShowNewPostModal={setShowNewPostModal}
+      />
     </div>
   );
 }
